76dae9d2e506df46224c2a7763a0333dd2883acc
Yassine BEKIOUI <mp482a@hotmail.fr>
2023-12-09 22:32:21 +0100

    finished all exercices, not tested yet

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,13 +8,12 @@ resource "azurerm_resource_group" "rg_esgi" {
 }
 
 resource "azurerm_container_registry" "acr_esgi" {
-  name                = "acr${var.student_name}"
+  name                = "acrTest${var.student_name}"
   resource_group_name = azurerm_resource_group.rg_esgi.name
   location            = azurerm_resource_group.rg_esgi.location
   sku                 = "Standard"
   admin_enabled       = true
 }
-
 
 resource "azurerm_kubernetes_cluster" "aks_esgi" {
   name                = "aks-${var.student_name}"
@@ -44,25 +43,16 @@ resource "azurerm_public_ip" "public_ip" {
   depends_on = [azurerm_kubernetes_cluster.aks_esgi]
 }
 
+resource "azurerm_role_assignment" "acr_pull_role" {
+  scope                = azurerm_container_registry.acr_esgi.id
+  principal_id         = azurerm_kubernetes_cluster.aks_esgi.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  depends_on           = [azurerm_kubernetes_cluster.aks_esgi]
+}
 
-# resource "azurerm_role_assignment" "acr_pull_role" {
-#   scope                = azurerm_container_registry.acr_esgi.id
-#   principal_id         = azurerm_kubernetes_cluster.aks_esgi.identity[0].principal_id
-#   role_definition_name = "AcrPull"
-#   depends_on           = [azurerm_kubernetes_cluster.aks_esgi]
-# }
-
-# // AcrPush
-# resource "azurerm_role_assignment" "acr_push_role" {
-#   scope                = azurerm_container_registry.acr_esgi.id
-#   principal_id         = azurerm_kubernetes_cluster.aks_esgi.identity[0].principal_id
-#   role_definition_name = "AcrPush"
-#   depends_on           = [azurerm_kubernetes_cluster.aks_esgi]
-# }
-
-# resource "azurerm_role_assignment" "acr_pull_role" {
-#   scope                = azurerm_container_registry.acr_main.id
-#   principal_id         = azurerm_kubernetes_cluster.aks_esgi.kubelet_identity.0.object_id
-#   role_definition_name = "AcrPull"
-#   depends_on           = [azurerm_kubernetes_cluster.aks_esgi]
-# }
+resource "azurerm_role_assignment" "acr_push_role" {
+  scope                = azurerm_container_registry.acr_esgi.id
+  principal_id         = azurerm_kubernetes_cluster.aks_esgi.identity[0].principal_id
+  role_definition_name = "AcrPush"
+  depends_on           = [azurerm_kubernetes_cluster.aks_esgi]
+}
